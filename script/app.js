8bedcc2639c0ac9c5af5b979c4a11b84f5566648
Christiano David <christianod97@gmail.com>
2022-09-04 18:16:35 +0300

    fix image on prod

--- script/app.js
+++ script/app.js
@@ -1,9 +1,1 @@
 
-const peoples = () =>{
-    let avatars = [0,1,2,3].map(item => (
-        `<img src="../assets/avatar-${item}.jpg" class="avatar avatar-head" alt="">`
-    )).join("")
-    let element = document.getElementById("people-avatar")
-    element.innerHTML = avatars+`<div class="avatar avatar-head centerJustify centerALign plus"><i class="fa-solid fa-plus"></i></div>`
-}
-peoples()
