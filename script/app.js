c4f863beebc2452af01feab984f176ee13daf387
Christiano David <christianod97@gmail.com>
2022-09-04 14:09:41 +0300

    integration ok

---
+++ script/app.js
@@ -0,0 +1,9 @@
+
+const peoples = () =>{
+    let avatars = [0,1,2,3].map(item => (
+        `<img src="../assets/avatar-${item}.jpg" class="avatar avatar-head" alt="">`
+    ))
+    let element = document.getElementById("people-avatar")
+    element.innerHTML = avatars+`<div class="avatar avatar-head centerJustify centerALign plus"><i class="fa-solid fa-plus"></i></div>`
+}
+peoples()
